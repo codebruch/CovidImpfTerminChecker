703fef0a7cb28e572606c4fa16e588488798be55
Frood <froofproton@protonmail.com>
2021-02-03 20:02:44 +0100

    errey

--- getImpfTermin.py
+++ getImpfTermin.py
@@ -15,7 +15,6 @@ import time
 import locale
 import requests
 import argparse
-import socketio
 import json
 import pandas as pd
 
@@ -38,65 +37,72 @@ path = os.path.dirname(os.path.abspath(__file__))
 prefs = {"download.default_directory":path}
 
 
-
 chrome_options.add_experimental_option("prefs", prefs)
 
-
-driver = webdriver.Chrome(r"C:\\Users\\d047102\\Desktop\\StockIntelligenceDataService\\chromedriver.exe",options=chrome_options) #C:\\Users\\d047102\\Desktop\\DemoDataGrabber
+driver = webdriver.Chrome(executable_path='/Users/d039861/Documents/Developments/corona/chromedriver')
 #//*[@id="app--idDemoSearchField-inner"]
 
-driver.get("https://001-iz.impfterminservice.de/impftermine/service?plz=69124")
+urls = ["https://005-iz.impfterminservice.de/impftermine/service?plz=71636", 
+		"https://001-iz.impfterminservice.de/impftermine/service?plz=69124",
+		 "https://001-iz.impfterminservice.de/impftermine/service?plz=74889",
+		 "https://001-iz.impfterminservice.de/impftermine/service?plz=77656",
+		 "https://001-iz.impfterminservice.de/impftermine/service?plz=69469",
+		 "https://229-iz.impfterminservice.de/impftermine/service?plz=70629"]
+
+
 #driver.maximize_window()
 
 #/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[2]/div/div/label[2]/span
 TerminFound = True
 while TerminFound:
-    try: 
-        time.sleep(2)   
-        cookiesButton = driver.find_elements_by_xpath('.//html/body/app-root/div/div/div/div[2]/div[2]/div/div[1]/a')
-        if len(cookiesButton) > 0:
-            cookiesButton[0].click()
-            print(str(datetime.now()) + " - cookiesButton click")
-        else:
-            print(str(datetime.now()) + " - cookiesButton notfound")
+    for url in urls:
+        if TerminFound == False:
+            break
+        driver.get(url)
+        try: 
+            time.sleep(2)   
+            cookiesButton = driver.find_elements_by_xpath('.//html/body/app-root/div/div/div/div[2]/div[2]/div/div[1]/a')
+            if len(cookiesButton) > 0:
+                cookiesButton[0].click()
+                print(str(datetime.now()) + " - cookiesButton click")
+            else:
+                print(str(datetime.now()) + " - cookiesButton notfound")
 
 
         
 
-        neinButton = WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.XPATH, './/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[2]/div/div/label[2]/span')))
-        neinButton.click()
-        print(str(datetime.now()) + " - neinButton click")
-        count = 0
-        while count < 3:
-            response = WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.XPATH, './/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div')))
-           
-
-            try:  
-                txtTmp = response.text
+            neinButton = WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.XPATH, './/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[2]/div/div/label[2]/span')))
+            neinButton.click()
+            print(str(datetime.now()) + " - neinButton click")
+            count = 0
+            while count < 3:
+                response = WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.XPATH, './/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div')))
+                try:  
+                    txtTmp = response.text
               
-            except StaleElementReferenceException as e:
-                print(e)
-                continue  
+                except StaleElementReferenceException as e:
+                    print(e)
+                    continue  
 
-            print(str(datetime.now()) + ' - Antwort: ' + txtTmp +   str(count))
+                print(str(datetime.now()) + ' - Antwort: ' + txtTmp +   str(count))
             
-            time.sleep(3)
-            count = count + 1
-            if txtTmp != 'Bitte warten, wir suchen verfügbare Termine in Ihrer Region.':
-                if txtTmp == 'Es wurden keine freien Termine in Ihrer Region gefunden. Bitte probieren Sie es später erneut.\n\nSobald genügend Impfstoff und die entsprechenden Kapazitäten vorhanden sind, werden die Impfzentren weitere Termine einstellen.':
-                    print(str(datetime.now()) + " - Kein Termin")
-                    count = 4
+                time.sleep(3)
+                count = count + 1
+                if txtTmp != 'Bitte warten, wir suchen verfügbare Termine in Ihrer Region.':
+                    if txtTmp == 'Es wurden keine freien Termine in Ihrer Region gefunden. Bitte probieren Sie es später erneut.\n\nSobald genügend Impfstoff und die entsprechenden Kapazitäten vorhanden sind, werden die Impfzentren weitere Termine einstellen.':
+                        print(str(datetime.now()) + " - Kein Termin")
+                        count = 4
 
-                else:
-                    print(str(datetime.now()) + " - termin?" )
-                    print(str(txtTmp))
-                    TerminFound = False
+                    else:
+                        print(str(datetime.now()) + " - termin?" )
+                        print(str(txtTmp))
+                        TerminFound = False
 
+            
+        except (TimeoutException):
+            print(str(datetime.now()) + " - timeout nein button not found")
 
-    except (TimeoutException):
-        print(str(datetime.now()) + " - timeout nein button not found")
-
-
-print(str(datetime.now()) + " - termin?" )
+        
+    print(str(datetime.now()) + " - termin?" )
 while True:
-    time.sleep(20000)   
+    	time.sleep(20000)   
